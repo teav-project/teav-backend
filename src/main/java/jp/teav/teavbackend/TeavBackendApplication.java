584fd78005f815d9192b11f3cd025a60de4a4316
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-03-12 01:47:00 +0900

    [add]springdocの追加、Projectパッケージの追加

--- src/main/java/jp/teav/teavbackend/TeavBackendApplication.java
+++ src/main/java/jp/teav/teavbackend/TeavBackendApplication.java
@@ -1,9 +1,12 @@
 package jp.teav.teavbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Teav API", description = "This service matches people who want to develop teams with each other.", version = "v1"))
 public class TeavBackendApplication {
 
     public static void main(String[] args) {
