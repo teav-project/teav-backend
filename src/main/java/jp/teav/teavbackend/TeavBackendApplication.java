0386d960cb62637d333279c4830ef726934fc551
ryusuke.arata <ryusuke.arata1130@gmail.com>
2023-03-11 18:04:11 +0900

    init commit.

---
+++ src/main/java/jp/teav/teavbackend/TeavBackendApplication.java
@@ -0,0 +1,13 @@
+package jp.teav.teavbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeavBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeavBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/teav/teavbackend/TeavBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.teav.teavbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeavBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
